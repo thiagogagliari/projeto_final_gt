555b6fdd50c2db9dcbe2de64589128c0ddf937ca
unknown <franciscosilva0023@digitalcollege.internal>
2025-05-12 11:28:58 -0300

    Instalando React

---
+++ app-react-projeto-final/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+function Login(){
+    return(
+        <div className="bg-blue-700">
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
