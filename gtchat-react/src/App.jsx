eb247b29f652d3b947349113a84baf707eff0f97
Fulano de Tal <avexedev@gmail.com>
2025-05-12 20:31:40 -0300

    Comecei a desenvolver os arquivos reacts

---
+++ gtchat-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './pages/Home';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <App/>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
